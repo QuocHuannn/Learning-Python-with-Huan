7bdd2c49b9d95350b351b13c53debb382872cc0d
QuocHuannn <truonghuan0709@gmail.com>
2022-08-24 19:31:48 +0700

    tuần 1 ( Ngày 1 )

---
+++ Week 1/HomeWork/main.py
@@ -0,0 +1,32 @@
+# CÂU 1 : DÙNG LỆNH PRINT() ĐỂ IN RA MÀN HÌNH CÁC HÌNH VẼ SAU:
+#                             *****************
+#                             *****************
+#                             *****************
+#                             *****************
+#                             *****************
+
+
+
+
+
+
+
+
+
+
+
+# CÂU 2 SỬ DỤNG HÀM INPUT() NHẬP TỪ BÀN PHÍM HAI SỐ TỰ NHIÊN A VÀ B. ĐƯA RA HIỆU A - B
+
+
+
+
+
+
+
+
+
+# CÂU 3 SỮA LỖI ĐOẠN CODE SAU
+# x = 2020
+# 2020 = x 
+# print(x)
+
---
+++ Week 1/Study/main.py
@@ -0,0 +1,62 @@
+# Sử dụng gán cứng để in ra màn hình
+# print("*\n**\n***\n****\n*****")
+ 
+# sử dụng bằng cách gán biến phụ
+# x = " Chao ban "
+# print(x)
+
+# dấu phẩy
+# print("Toi ten la" ,",", "Lam Trung hieu")
+
+#Với tham số sep
+# print("a","b","c", sep = ",")
+
+# với tham số end
+# print("a", end = " ")
+# print("b", end = " + ")
+# print("sai")
+# print("dung", end = " - ")
+# print("abcd")
+
+# sử dụng input
+# x = input(print("moi nhap a: "))
+# print(x)
+
+# tính tổng
+# a = int(input("Nha so a: "))
+# b = int(input("Nha so b: "))
+# print("Tong la: ", a+b)
+
+# gán giá trị cho biến x
+# x = " Hoc Python co ban "
+# print(x)
+# print(" Dia chi o nho chua bien x la: ", id(x))
+
+# x = " Hoc Python co ban "
+# print(x)
+# print(" Dia chi o nho chua bien x la: ", id(x))
+
+# x = " Da thay doi gia tri "
+# print("Gia tri bien x", x)
+# print("Dia chi cua bien x ", id(x))
+
+# # gán thông thường
+# a = 1
+# b = 1
+# c = 1
+# # rút gọn
+# a = b = c = 1
+
+# # gán thông thường
+# a = 1
+# b = 2
+# c = 3
+# # rút gọn
+# a, b, c = 1,2,3
+
+# # gán thông thường
+# x = a 
+# a = b 
+# b = x 
+# # rút gọn
+# a,b = b,a
