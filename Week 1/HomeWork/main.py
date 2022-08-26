f8a2828f8ff794cd13ed876b9a89d0eedab90edc
LamHieu1202 <lamtrunghieu1202@gmail.com>
2022-08-26 16:56:40 +0700

    Hieu nop bai
    Nop bai buoi 1

--- Week 1/HomeWork/main.py
+++ Week 1/HomeWork/main.py
@@ -6,17 +6,15 @@
 #                             *****************
 
 
+print("*****************\n*****************\n*****************\n*****************\n*****************")
 
-
-x = " day la cach nop bai "
-print(x)
-abcdef
 
 
 
 
 # CÂU 2 SỬ DỤNG HÀM INPUT() NHẬP TỪ BÀN PHÍM HAI SỐ TỰ NHIÊN A VÀ B. ĐƯA RA HIỆU A - B
-
+A = int(input("nhập số tự nhiên A :"))
+B = int(input("nhập số tự nhiên B :"))
 
 
 
@@ -30,3 +28,6 @@ abcdef
 # 2020 = x 
 # print(x)
 
+
+x = 2020 
+print(x)
