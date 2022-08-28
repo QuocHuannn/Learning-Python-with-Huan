874ba63447ccc682e473ee225f31dcc0081f904e
QuocHuannn <truonghuan0709@gmail.com>
2022-08-28 19:52:18 +0700

    Update bài tập tuần 1
    Sửa lại bài 2 ở main1 và hoàn thành main2 trước buổi học tiếp theo

---
+++ Week 1/HomeWork/main2.py
@@ -0,0 +1,11 @@
+#################################################
+            #  Kiểu Dữ Liệu 
+#################################################
+
+# Câu 1: Nhập số nguyên dương a có 3 chữ số. Tính tổng các chữ số của a ( ví dụ nhập a = 8 thì tổng của a là 36)
+
+# Câu 2:Cho đường tròn có bán kính R. Tính chu vi của đường tròn đó, kết quả làm tròn đến 2 chữ số thập phân
+
+# Câu 3: Nhập ba số nguyên dương a,b,c. Hãy tìm ước chung lớn nhất của 3 số a,b,c. ** Gợi ý: dùng hàm gcd(a,b) trong module math để tìm ước chung lớn nhất của 2 số a và b
+
+# Câu 4: Nhập 2 số nguyên dương a,b. Tìm bội chung nhỏ nhất của a và b. ** Gợi ý: BCNN của (a,b) = a*b//gcd(a,b)
