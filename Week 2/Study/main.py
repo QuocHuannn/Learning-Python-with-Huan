ace1987d0691689eea58f84d3c1e911dcc0d3734
QuocHuannn <truonghuan0709@gmail.com>
2022-08-31 19:32:11 +0700

    Tuần 2

---
+++ Week 2/Study/main.py
@@ -0,0 +1,74 @@
+# # VD1: 
+# a = 10
+# b = 20
+# if a < b:
+#     print("a nhỏ hơn b")
+
+# #Vd2:
+# a = 10
+# b = 20
+# if a < b:
+#     print("a nhỏ hơn b")
+#     print("Tổng của a và b là:", a+b)
+
+# #VD3: 
+# a = 10
+# b = 20
+# if a > b:
+#     print("a nhỏ hơn b")
+#     print("Tổng của a và b là:", a+b)
+# print("Vị trí ngoài khối lệnh")
+
+
+# #VD4:
+# a = 20
+# b = 20
+# if a == b:
+#     print("a và b bằng nhau")
+# else:
+#     print("a và b khác nhau")
+    
+# #VD5:
+# a = 2
+# b = 20
+# if a == b:
+#     print("a và b bằng nhau")
+# else:
+#     print("a và b khác nhau")
+
+#Vd6:
+# a = 20 
+# b = 30
+# if a == b:
+#     print("a và b bằng nhau")
+# elif a < b:
+#     print("a nhỏ hơn b")
+    
+#vd7:
+# a = 200
+# b = 30
+# if a == b:
+#     print("a và b bằng nhau")
+# elif a < b:
+#     print("a nhỏ hơn b")
+# else:
+#     print("a lớn hơn b")
+
+# # VD8:
+# a = 10
+# b = 20
+# c = 15
+# if (a + b > c) and (b + c >a ) and (c + a > b):
+#     print(" a, b, c là ba cạnh của 1 tam giác")
+# else:
+#     print(" a, b, c ko là cạnh của 1 tam giác")
+    
+# #VD9:
+# a = 10
+# b = 11
+# if (a%2 == 0) or ( b%2 == 0 ):
+#     print("trong 2 số a, b có ít nhất 1 số chẵn")
+# else:
+#     print("2 số a,b đều là số lẻ")
+
+
