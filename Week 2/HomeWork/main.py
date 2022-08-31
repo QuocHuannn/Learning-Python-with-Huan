25cb8d6d70cbe194e17db8a8353cd28c3afe2d7e
QuocHuannn <truonghuan0709@gmail.com>
2022-08-31 22:44:48 +0700

    Bài tập

---
+++ Week 2/HomeWork/main.py
@@ -0,0 +1,16 @@
+# Bài 1: Giải phương trình bậc 2. Nhập 3 số a, b, c giải pt ax^2 +bx + c = 0
+
+
+# Bài 2: phân loại tam giác
+# Nhập vào 3 số a, b, c nguyên dương. kiểm tra xem a,b,c có hải là 3 cạnh của tam giác không? Nếu là ba cạnh của tam giác thì hãy phân loại xem tác giác đó thuộc loại nào
+# 1. tam giác đều ||  2. tam giác vuông || 3. tam giác thường
+
+
+# Bài 3: Xếp loại học sinh
+# cho điểm thi Toán, Lý, Hóa, Sinh, Tin của học sinh. Hãy xếp loại kết quả của học sinh theo tiêu chí:
+# 9 <= ĐTB ( điểm trung bình ) thì xếp loại xuất sắc
+# 8 <= ĐTB < 9 ( điểm trung bình ) thì xếp loại giỏi
+# 7 <= ĐTB < 8 ( điểm trung bình ) thì xếp loại khá
+# 5 <= ĐTB < 7 ( điểm trung bình ) thì xếp loại trung bình
+# 5 < ĐTB( điểm trung bình ) thì xếp loại yếu
+
