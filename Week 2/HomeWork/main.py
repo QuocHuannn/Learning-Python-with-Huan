15002380202f9c716d1640e6142388edbaa3db2a
LamHieu1202 <lamtrunghieu1202@gmail.com>
2022-09-02 10:48:14 +0700

    hieu
    nop bai tuan 2

--- Week 2/HomeWork/main.py
+++ Week 2/HomeWork/main.py
@@ -1,4 +1,21 @@
 # Bài 1: Giải phương trình bậc 2. Nhập 3 số a, b, c giải pt ax^2 +bx + c = 0
+from math import sqrt 
+a = float(input(" nhập số a :" ))
+b = float(input(" nhập số b :" ))
+c = float(input(" nhập số c :" ))
+delta = b**2 - ( 4*a*c )
+if delta < 0 :
+    print (" phương trình vô nghiệm ")
+elif delta == 0 :
+    print (" phương trình có nghiệm kép x1 = x2 = "  , -b/(2*a))
+else : 
+    x1 = (-b + sqrt(delta))/(2*a) 
+    x2 = (-b - sqrt(delta))/(2*a) 
+    print ("print phương trình có 2 nghiệm phân biệt " , x1 , x2  )
+
+    
+    
+    
 
 
 
