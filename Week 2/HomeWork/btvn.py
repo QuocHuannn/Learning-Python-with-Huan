b17be01403962c5130c8eca9a11e5e22feb1bf25
LamHieu1202 <lamtrunghieu1202@gmail.com>
2022-09-06 21:35:03 +0700

    hieu
    nop bai thieu bai 23

---
+++ Week 2/HomeWork/btvn.py
@@ -0,0 +1,57 @@
+#p11
+# a = 2 
+# b = 7
+# c = 5
+# if b > a and b > c :
+#     print(a,c,b)
+    
+    
+    
+    
+#p12
+# a = 2
+# b = 7
+# c = 5
+# d = 12
+# print ( d , a )
+
+
+
+
+
+#p18
+# hour = int(input("nhập giờ bạn muốn xem phim " ))
+# 0 <= hour <= 23
+# day = int(input("nhập ngày bạn muốn xem phim " ))
+# 1 <= day <= 8 
+# if  day == (2,4,5) and  hour  < 17 :
+#     print ("giá vé là 60000đ")
+# elif day ==(2,4,5) and hour > 17 :
+#     print (" giá vé là 700000đ")
+# elif day == 3 :
+#     print (" happy day giá vé là 50000đ")
+# elif day == (6,7,8) and hour < 17 :
+#     print ("giá vé là 75000đ")
+# else :
+#     print (" giá vé là 80000đ")
+
+
+
+
+
+#p20
+# km = float(input(" nhập số km đi được "))
+# 0 < km <= 10**9
+# if km <= 1 :
+#     print (" số tièn taxi phải trả là :" , km * 15000)
+# elif 2 <= km <= 5 :
+#     print ("số tiền taxi phải trả là :" , km * 13500)
+# elif 5 < km < 12 :
+#     print (" số tiền taxi phải trả là :" , km * 11000 )
+# else :
+#     print (" số tiền phải trả là :" , km * 11000 - km * 1100 )
+
+
+
+
+    
