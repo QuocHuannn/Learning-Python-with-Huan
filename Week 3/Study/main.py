017a01ed8ae377cc33f1028e17684d4578826112
QuocHuannn <truonghuan0709@gmail.com>
2022-09-09 16:15:06 +0700

    week3

---
+++ Week 3/Study/main.py
@@ -0,0 +1,74 @@
+# # VD1:
+# for x in 1,2,3:   
+#     print(x)
+
+# # VD2:
+# a = 10
+# b = 20
+# c = 30
+# s = 0
+
+# for x in a, b, c :
+#     s = s + x  # s = s + x (a , b , c) 
+#     # s = s + 10 ----> s = 10
+#     # s = s + 20 ----> s = 30
+#     # s = s + 30 ----> s = 60
+# print("Tổng của a + b + c là : ", s)
+
+# # VD3:
+# for x in range (3):
+#     print("Học Python")
+
+# # VD4:
+# for x in range(3):
+#     print('x = ', x)
+
+# # VD5:
+# for x in range (2 , 5):
+#     print("x = ",x)
+
+# # Vd6:
+# for x in range (2,5,2):
+#     print("x = ",x)
+
+# # VD7:
+# for x in range (4,10,0):
+#     print('x = ', x)
+
+# # VD8:
+# for x in range (4,-10,-2):
+#     print('x = ', x)
+
+# # VD9:
+# n = 1
+# while n <= 3 :
+#     print("n = ", n)
+#     n = n + 1
+    
+# #Vd10:
+# n = 4
+# while n <= 3:
+#     print("n = ",n)
+#     n = n + 1
+# print("ngoài vòng while")
+
+# # VD11:
+# for x in 1 ,2 ,3, 4 ,5:
+#     if( x % 2 == 0 ):
+#         continue
+#     print("x = ", x , " là số lẻ")
+
+# #VD12:
+# for x in 1,2,3,4,5: 
+#     if(x == 4):
+#         break
+#     print("x = ",x)
+# print("thoát")
+
+#VD13:
+# for x in range(3):
+#     for y in range(6):
+#         if (y == 3):
+#             break
+#         print("x, y:",x,y,sep = ' ')
+        
