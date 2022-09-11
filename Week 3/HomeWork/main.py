0673de7cb87d4ff8f531a34baf3a9268c770c784
QuocHuannn <truonghuan0709@gmail.com>
2022-09-11 12:35:54 +0700

    Create main.py

---
+++ Week 3/HomeWork/main.py
@@ -0,0 +1,33 @@
+# SỬ DỤNG CẤU TRÚC LẶP
+# Cho một số tự nhiên n (n <= 10^1000).
+# Tính tổng các chữ số của n.
+# vd: n = 123. Tổng các chữ số bằng 1 + 2 + 3 = 6
+
+
+
+
+
+
+
+
+
+
+# Cho một số tự nhiên n (n <= 10^1000).
+# TÌm chữ số lớn nhất và nhỏ nhất
+# VD: n = 123 . Min = 1 & Max = 3
+
+
+
+
+
+
+# Tìm ước chung lớn nhất và bội chung nhỏ nhất của 2 số a và b
+# VD: a = 9 và b = 6 thì ước chung lớn nhất bằng 3, bội chung nhỏ nhất bằng 18
+
+
+
+
+
+
+
+# Kiểm tra số nguyên tố, cho 1 số n < 10^9. Kiểm tra xem nó có phải là số nguyên tố k
