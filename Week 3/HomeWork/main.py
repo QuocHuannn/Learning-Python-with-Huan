80b0949c4c76518861dcef04e8b34fb8022021c3
LamHieu1202 <lamtrunghieu1202@gmail.com>
2022-10-13 08:05:44 +0700

    hieu
    nop bai

--- Week 3/HomeWork/main.py
+++ Week 3/HomeWork/main.py
@@ -36,6 +36,17 @@
 
 # Tìm ước chung lớn nhất và bội chung nhỏ nhất của 2 số a và b
 # VD: a = 9 và b = 6 thì ước chung lớn nhất bằng 3, bội chung nhỏ nhất bằng 18
+# a = int(input("Nhập số  a = "))
+# b = int(input("Nhập số  b = "))
+# def ucln(a, b):
+#     if (b == 0):
+#         return a;
+#     return ucln(b, a % b);
+# def bcnn(a, b):
+#     return int((a * b) / ucln(a, b));
+# print("Ước  chung lớn nhất của a và b là : " , ucln(a,b))
+# print("Bội chung nhỏ nhất của a và b là : " , bcnn(a,b))
+
 
 
 
@@ -45,24 +56,24 @@
 
 
 # Kiểm tra số nguyên tố, cho 1 số n < 10^9. Kiểm tra xem nó có phải là số nguyên tố k
-def kiemtra_songuyento(n):
-    count = 0
-    for i in range(1 , n + 1):
-        if n < 2:
-            print(True)
-        elif n==2:
-            print(False)    
-        elif n % 2:
-            print(False)    
-        else:
-            for i in range (3 , n , 2):
-                if i%n == 0:
-                    print(False) 
-n = int(input("moi ban nhap 1 so bat ky: "))
-if n ==  True:
-    print("la so nguyen to")
-else:
-    print("khong phai so nguyen to")                   
+# def kiemtra_songuyento(n):
+#     count = 0
+#     for i in range(1 , n + 1):
+#         if n < 2:
+#             print(True)
+#         elif n==2:
+#             print(False)    
+#         elif n % 2:
+#             print(False)    
+#         else:
+#             for i in range (3 , n , 2):
+#                 if i%n == 0:
+#                     print(False) 
+# n = int(input("moi ban nhap 1 so bat ky: "))
+# if n ==  True:
+#     print("la so nguyen to")
+# else:
+#     print("khong phai so nguyen to")                   
 
      
         
